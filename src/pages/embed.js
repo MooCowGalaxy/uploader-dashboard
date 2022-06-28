19ef5596f0e00968d68125feb175c4e445a75b03
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-06-28 16:12:15 -0700

    fix embed toggle not working

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -104,8 +104,8 @@ function Embed() {
                     <div className="form-check form-switch">
                         <input
                             className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-                            type="checkbox" role="switch" checked={embedEnabled} id="page-embed-embed-toggle"
-                            onClick={() => setEmbedEnabled(x => !x)}/>
+                            type="checkbox" role="switch" defaultChecked={embedEnabled} id="page-embed-embed-toggle"
+                            onChange={e=> setEmbedEnabled(e.target.checked)}/>
                         <label className="form-check-label inline-block text-gray-800"
                                htmlFor="page-embed-embed-toggle">Enable embeds</label>
                     </div>
