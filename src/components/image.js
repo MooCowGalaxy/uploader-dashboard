032ea7694c5a85d45062d7cb453998e8dc8a4aa0
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-06-28 19:53:47 -0700

    fix download button not pointing to cdn

--- src/components/image.js
+++ src/components/image.js
@@ -16,15 +16,6 @@ function Image({image, domain, del}) {
         ))
     }
 
-    const onDownload = () => {
-        const link = document.createElement("a")
-        link.download = `${image.fileId}.${image.extension}`
-        link.href = `https://cdn.uploader.tech/${user.settings.userId}/${image.fileId}.${image.extension}`
-        document.body.appendChild(link)
-        link.click()
-        document.body.removeChild(link)
-    }
-
     const onDelete = () => {
         setDeleteState(1)
         sendReq(`/api/user/image/delete/${image.fileId}`, {method: 'POST'})
@@ -60,16 +51,17 @@ function Image({image, domain, del}) {
                             Copy Link
                         </button>
                     </CopyToClipboard>
-                    <button className="bg-green-600 rounded-md pl-3 px-4 py-2 text-white mb-1 mx-0.5"
-                            onClick={onDownload}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 -translate-y-0.5 inline"
-                             fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
-                        </svg>
-                        &nbsp;
-                        Download
-                    </button>
+                    <a href={`https://cdn.uploader.tech/${user.settings.userId}/${image.fileId}.${image.extension}`} target="_blank" rel="noopener noreferrer" download>
+                        <button className="bg-green-600 rounded-md pl-3 px-4 py-2 text-white mb-1 mx-0.5">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 -translate-y-0.5 inline"
+                                 fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
+                            </svg>
+                            &nbsp;
+                            Download
+                        </button>
+                    </a>
                     <button className="bg-red-600 rounded-md pl-3 px-4 py-2 text-white mb-1 disabled:opacity-75 mx-0.5"
                             onClick={onDelete} disabled={deleteState === 1}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 -translate-y-0.5 inline"
