d787777bc0f4bcabbf2330f83dd7e453ba1605bc
moo <moo@uploader.tech>
2023-10-11 21:05:46 -0700

    fix discord invite link in dashboard

--- src/vars.js
+++ src/vars.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.REACT_APP_BASE_URL
 export default baseUrl;
 export const production = process.env.REACT_APP_PRODUCTION === 'true'
-export const discordInvite = "https://discord.gg/FCA7XBdJqG"
+export const discordInvite = "https://discord.gg/CyvQaJ2Kz5"
